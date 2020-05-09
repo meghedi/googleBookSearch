445fe0c414f6252b87dace7e417ba266be2987a4
Meghedi Hacopi <hacopian.m@gmail.com>
2020-05-09 13:20:02 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,11 +8,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
-app.use(routes);
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT} !`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
